96ecf3d39462b55d51507dbfe29620c5ae50d7d1
jimspi <jpsthesecond@gmail.com>
2025-08-06 21:39:21 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
+      'firebasestorage.googleapis.com'
+    ],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+};
+
+module.exports = nextConfig;
