cd323b717f4638eff927a2c23f82082a4efdd129
jimspi <jpsthesecond@gmail.com>
2025-08-06 21:57:34 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function App({ Component, pageProps }) {
+  const { session, ...otherPageProps } = pageProps;
+  
   return (
-    
-      
-    
+    <SessionProvider session={session}>
+      <Component {...otherPageProps} />
+    </SessionProvider>
   );
 }
